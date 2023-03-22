0d0dcf810e72317d2f5840c3d3bd1d4004244dc3
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-03-22 18:48:05 +0900

    refactor: useEffect
    - useEffect 안에서 If문, state변경함수 활용

--- app/src/components/Loading.jsx
+++ app/src/components/Loading.jsx
@@ -2,45 +2,47 @@ import React, { useState, useEffect } from 'react'
 
 function Loading() {
     const [isLoading, setIsLoading] = useState(false)
+    const [location, setLocation] = useState()
+    const [detailLocation, setDetailLocation] = useState([])
 
-    const [location, setLocation] = useState('')
+    useEffect(() => {
+        if (location === '서울') {
+            setDetailLocation(['서초구양재동'])
+        } else if (location === '부산') {
+            setDetailLocation(['해운대바다'])
+        }
+    }, [location])
     useEffect(() => {
         setTimeout(() => {
             setIsLoading(true)
         }, 0.4)
     }, [])
 
-    return <div>{isLoading === true ? <IsLoaded /> : <>로딩중</>}</div>
-}
-function IsLoaded() {
-    const [isClicked, setIsClicked] = useState(false)
-    const [location, setLocation] = useState('')
-    const [detailLocation, setDetailLocation] = useState([])
     return (
         <div>
-            <div>
-                로딩완료!
-                <button
-                    onClick={(e) => {
-                        setIsClicked(true)
-                        setLocation(e.target.textContent)
-                        setDetailLocation(['서초구양재동'])
-                    }}
-                >
-                    서울
-                </button>
-                <button
-                    onClick={(e) => {
-                        setIsClicked(true)
-                        setLocation(e.target.textContent)
-                        setDetailLocation(['해운대바다'])
-                    }}
-                >
-                    부산
-                </button>
-            </div>
-            <div>현재 내 지역:{location}</div>
-            <div>세부지역:{detailLocation}</div>
+            {isLoading === true ? (
+                <>
+                    <button
+                        onClick={() => {
+                            setLocation('서울')
+                        }}
+                    >
+                        서울
+                    </button>
+                    <button
+                        onClick={() => {
+                            setLocation('부산')
+                        }}
+                    >
+                        부산
+                    </button>
+
+                    <div>현재 내 지역:{location}</div>
+                    <div>세부지역:{detailLocation}</div>
+                </>
+            ) : (
+                <>로딩중</>
+            )}
         </div>
     )
 }
